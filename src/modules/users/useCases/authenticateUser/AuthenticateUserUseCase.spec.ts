af3d18c1f954129f2656e1cd052c4e96b6d0e358
Thiago <gmoliveira69@gmail.com>
2022-07-17 22:55:51 -0300

    authenticate user test

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,58 @@
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { AppError } from "@shared/errors/AppError";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+describe("It should authenticate user", () => {
+  let inMemoryUsersRepository: IUsersRepository;
+  let authenticateUserUseCase: AuthenticateUserUseCase;
+  let createUserUseCase: CreateUserUseCase;
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository);
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should authenticate a user", async () => {
+    const user = {
+      email: "teste@email.com",
+      name: "teste",
+      password: "123456",
+    };
+    await createUserUseCase.execute(user);
+
+    const session = await authenticateUserUseCase.execute({email: user.email, password: user.password})
+
+    expect(session.user).toHaveProperty("id");
+    expect(session.token)
+  });
+
+  it("should not find user with a incorrect email", async () => {
+    const user = {
+      email: "testando@email.com",
+      name: "teste",
+      password: "123456",
+    };
+    await createUserUseCase.execute(user);
+  
+
+    await expect(authenticateUserUseCase.execute({email: 'fake@gmail.com', password: user.password})).rejects.toEqual(
+      new AppError("Incorrect email or password", 401)
+    );
+  });
+
+  it("should not find user with a incorrect password", async () => {
+    const user = {
+      email: "testando@email.com",
+      name: "teste",
+      password: "123456",
+    };
+    await createUserUseCase.execute(user);
+  
+
+    await expect(authenticateUserUseCase.execute({email:user.email, password: "12345"})).rejects.toEqual(
+      new AppError("Incorrect email or password", 401)
+    );
+  });
+});
